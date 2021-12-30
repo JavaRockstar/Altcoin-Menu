c51618660cce3fc888a8c23cc5f9503d8b893750
HackingVision <craigyhaze@gmail.com>
2021-12-30 06:18:36 +0000

    Create mixed-algo.sh

---
+++ mixed-algo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Check if user is root 
+
+while [ "$(whoami)" != "root" ]; do
+    echo "You must be root!"
+    exit 1
+done
+
+#Terminal Colors
+
+grn=$'\x1b[32m'
+red=$'\x1b[31m'
+pur=$'\x1b[35m'
+ylw=$'\x1b[33m'
+rst=$'\x1b[0m'
