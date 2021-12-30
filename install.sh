746a1dcde28d9da54a0e9fee85b27b48145a3fb4
HackingVision <craigyhaze@gmail.com>
2021-12-30 04:15:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,112 +15,42 @@ pur=$'\x1b[35m'
 ylw=$'\x1b[33m'
 rst=$'\x1b[0m'
 
-# List of wallet addresses that will be echo to crytomining software config file
+#!/bin/bash
+# Bash Menu Script Example
 
-function select_listed_wallet
-{
-    printf '\n List Of Saved Wallet Addresses \n'
-    count=0
-    declare -a wallet_array=()
-    while read -r line; do
-        count=$(expr $count + 1)
-        echo "[$count] $line"
-        wallet_array+=($line)
-    done < walletList.txt
+base64 -d <<<"H4sIAAAAAAAAA43QwQ0AIQgEwD/FGKqxli3/FMmJouC+wIwJUEoPg7lkob9iZJ5s03j78GYLQmwt
+t4DvmBapk1ww7VLwSTLISek3JkuL3aU+jYzjqPWSl2CxkTQU6309jWxAM4vIXhe7W5ziZzgyZyNn
+beamfYBq32SztadXNQ3lZNoPpitpmfQDAAA=
+" | gunzip
 
-    if [ $count -eq 0 ]; then
-        echo '--- No Wallets Found ---'
-    fi
+echo -e "Shuffz Mining Script \n" 
 
-    printf '\nJust Testing ? Select A Random Wallet from this list : '
-    read mac_index
-
-    # validate input value 0 < mac index < count
-    if [ $mac_index -le 0 ] || [ $mac_index -gt $count ]; then
-        echo 'ERROR : Wrong Index Selected'
-        exit
-    fi
-
-    mac_index=$(($mac_index - 1))
-    change_mac ${mac_array[$mac_index]}
-}
-
-function select_random_mac
-{
-    count=0
-    declare -a mac_array=()
-    while read -r line; do
-        count=$(expr $count + 1)
-        mac_array+=($line)
-    done < macList.txt
-
-    if [ $count -eq 0 ]; then
-        echo '--- No MACs Found ---'
-    fi
-
-    # Generate random number b/w 0 & count
-    mac_index=$((RANDOM % count))
-
-    # Change MAC
-    change_mac ${mac_array[$mac_index]}
-}
-
-function reset_default_mac
-{
-    printf '\n*** Restting MAC ***\n'
-    ip link set $if_name down
-    macchanger -p $if_name
-    ip link set $if_name up
-}
-
-function change_mac
-{
-    printf '\n*** Changing MAC ***\n'
-    ip link set $if_name down
-    macchanger -m $1 $if_name
-    ip link set $if_name up
-}
-
-# /////////////////////////
-# Program Starts From Here
-# /////////////////////////
-
-#check for root permission
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-#Get Interface Name From User
-printf "Enter Netwok Interface Name : "
-read if_name
-
-# validate input value 0 < interface name < 12
-if [ ${#if_name} -le 0 ] || [ ${#if_name} -gt 12 ]; then
-    echo 'ERROR : Not A Valid Input'
-    exit
-fi
-
-# While Menu
-while true; do
-    printf "\n[ MAC Selector ]\n"
-    printf '\n1) Select MAC from List'
-    printf '\n2) Set Random MAC From List'
-    printf '\n3) Reset Default MAC'
-    printf '\n\nChoose Other Option To Exit [ Select Option ] : '
-    read option
-
-    if [ "$option" = "1" ]; then
-        select_listed_mac
-        exit
-    elif [ "$option" = "2" ]; then
-        select_random_mac
-        exit
-    elif [ "$option" = "3" ]; then
-        reset_default_mac
-        exit
-    else
-        echo 'Exiting...'
-        exit
-    fi
+PS3='Please enter your choice: '
+options=("Mine ETH" "Mine ERG" "Mine Firo" "Mine Flux" "NiceHash" "Donate Core" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Mine ETH")
+            echo "you chose choice 1"
+            ;;
+        "Mine ERG")
+            echo "you chose choice 2"
+            ;;
+        "Mine Firo")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Mine Flux")
+            echo "you chose choice 2"
+            ;;
+        "Mine XMR")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Mine VRSC")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
 done
